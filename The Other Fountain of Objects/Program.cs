0ac1079002ead0d43a6fbc040e15ae111531f129
ClintonSBarnes <clintbarnes8@gmail.com>
2022-06-25 20:29:22 -0500

    updated notes

--- The Other Fountain of Objects/Program.cs
+++ The Other Fountain of Objects/Program.cs
@@ -2,6 +2,6 @@
 Game game = new Game();
 game.Run();
 
-//Need to work on the board array.
-////It is currently not taking into account the starting position 0,0.
-//it is assuming a starting point of 1,1 when refering to player movement. 
+//remaining UNDONE:
+//maelstrom relocation system
+//arrow system
